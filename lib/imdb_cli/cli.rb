37a508a7a5b560c37eaaef5a993454b911e1ddba
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-19 21:31:24 +0000

    Added 2 methods: list_trailers & movie_info.

--- lib/imdb_cli/cli.rb
+++ lib/imdb_cli/cli.rb
@@ -12,8 +12,25 @@ class ImdbaseCli
       case u_i
       when "list"
         puts "Movie trailers listed."
+        movie_info
       when "exit"
         puts "Thank you for using gem!"
+      else
+        puts "Invalid Entry!"
+      end
+    end
+  end
+
+  def movie_info
+    u_i = nil
+    puts "For more info on movie, please enter corresponding number."
+    while u_i != "exit"
+      u_i = gets.strip.to_i
+      case u_i
+      when integer
+        #select corresponding movie
+      when "exit"
+        list_trailers
       else
         puts "Invalid Entry!"
       end
