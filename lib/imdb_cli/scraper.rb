76cb9edd2603cf0740e1feafa8fd7ecf326e645d
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-19 23:20:42 +0000

    titles & trailer_link scrape methods.

--- lib/imdb_cli/scraper.rb
+++ lib/imdb_cli/scraper.rb
@@ -1,9 +1,16 @@
 class Scraper
-  IMDB_URL = https://www.imdb.com/trailers
+  IMDB_URL = "https://www.imdb.com/trailers"
 
   def self.scrape_trailers
     html = open(IMDB_URL)
     doc = Nokogiri::HTML(html)
+
+    # binding.pry
+    movies = []
+    titles = doc.css(".trailer-caption").children.text.split("\n ")
+    trailer_link = doc.css(".trailer-image").collect {|tl| tl.css("a").attr("href").value}
+binding.pry
+    
 
   end
 
