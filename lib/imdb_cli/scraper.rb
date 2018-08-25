6813d59a5824d6924a95b9572a080a8007f657a6
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-25 19:12:42 +0000

    Cleaned up Scraper class... deleted unnecessary code.

--- lib/imdb_cli/scraper.rb
+++ lib/imdb_cli/scraper.rb
@@ -5,36 +5,27 @@ class Scraper
     html = open(IMDB_URL)
     doc = Nokogiri::HTML(html)
 
-    # binding.pry
-    movies = []
-
     titles = doc.css(".trailer-caption").children.text.split("\n ").reject{|t| t.empty? || t == "   "}
     trailer_links = doc.css(".trailer-image").collect {|tl| tl.css("a").attr("href").value}
     @movie_links = doc.css(".trailer-caption").collect {|ml| ml.css("a").attr("href").value}
 
-
     titles[1..100].each.with_index do |title, i|
       Movie.new(title, "https://www.imdb.com#{trailer_links[i]}", "https://www.imdb.com#{@movie_links[i]}")
-      # movies << {
-      #   :title => title,
-      #    :trailer_link => "https://www.imdb.com#{trailer_links[i]}",
-      #  :movie_info => movie_info[i]}
     end
-# binding.pry
   end
+end
 
-def self.scrape_movie_info
-  @movie_links.each do |movie_link|
-    html = open("https://www.imdb.com#{movie_link}")
-    doc = Nokogiri::HTML(html)
+# def self.scrape_movie_info
+#   Movie.all.each do |movie|
+#     html = open("#{movie.movie_info}")
+#     doc = Nokogiri::HTML(html)
 
-binding.pry
-
+# binding.pry
+  # title = doc.css(".title_wrapper h1").children[0].text.chop
   # year = doc.css(".title_wrapper a").children[0].text
   # genre = doc.css(".title_wrapper a").children[1].text
   # all genres = doc.css(".title_wrapper a").children[1..3].text
-  # start for summary = doc.css(".plot_summary")
-  end
-end
-
-end
+  # summary = doc.css(".summary_text").children.text.strip
+  # director = doc.css(".credit_summary_item a")[0].children.text
+#   end
+# end
