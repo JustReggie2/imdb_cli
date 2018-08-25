4a6990db08cd2b419ae8030a2700ad66b89066e2
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-25 19:00:08 +0000

    Added doc method, change accessor names.

--- lib/imdb_cli/movie.rb
+++ lib/imdb_cli/movie.rb
@@ -1,5 +1,6 @@
 class Movie
-  attr_accessor :title, :trailer_link, :movie_info, :director, :description, :rating
+  attr_accessor :title, :trailer_link, :movie_info, :director, :summary, :genre,
+  :year
 
   @@all = []
 
@@ -13,5 +14,11 @@ class Movie
   def self.all
     @@all
   end
+
+  def doc
+    @doc ||= Nokogiri::HTML(open(self.movie_info))
+  end
+
+  
 
 end
