4196fd21909804692f5b8c2cc5a8ede055a52241
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-26 00:46:07 +0000

    Cont. updating start method.

--- lib/imdb_cli/cli.rb
+++ lib/imdb_cli/cli.rb
@@ -7,29 +7,49 @@ class ImdbaseCli
   end
 
   def start
-    u_i = nil
+    # u_i = nil
     puts "What Top trailers would you like to see? 10, 25 or 50?"
-    while u_i != "exit"
       u_i = gets.strip.to_i
       case u_i
       when 10
-        list_trailers
+        puts ""
+        puts "Here are the current Popular Top 10 Trailers!"
+        list_trailers(10)
         movie_info
-      when "exit"
-        puts "Thank you for using gem!"
+      when 25
+        puts ""
+        puts "Here are the current Popular Top 25 Trailers!"
+        list_trailers(25)
+        movie_info
+      when 50
+        puts ""
+        puts "Here are the current Popular Top 50 Trailers!"
+        list_trailers(50)
+        movie_info
       else
         puts "Invalid Entry!"
       end
-    end
-    list_trailers
+    puts ""
+    puts "Would you like to see trailers list again? (Y/N)"
+
+    u_i = gets.strip.downcase
+      case u_i
+      when "y"
+        start
+      when "n"
+        puts ""
+        puts "Thank you for using IMDB's Trailer Gem!"
+      else
+        puts ""
+        puts "Invalid Entry! Please try again."
+        start
+      end
   end
 
   def list_trailers(num)
     # Movie.all.sort! {|a, b| a.title <=> b.title}
-    Movie.all.each_with_index(0..num-1) do |movie, index|
+    Movie.all[0..num-1].each_with_index do |movie, index|
       puts "#{index+=1}. #{movie.title}"
-        # puts movie.trailer_link
-        # puts movie.movie_info
     end
   end
 
