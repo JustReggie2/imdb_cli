7a40e02b908830bbba78db045f6b508232442fa5
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-26 23:38:38 +0000

    Cleaning up my enviroment.

--- lib/imdb_cli.rb
+++ lib/imdb_cli.rb
@@ -1,10 +1,14 @@
+# Required Gems #
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+module ImdbCli
+  # Your code goes here...
+end
+
+# Required Files #
 require_relative "imdb_cli/version"
 require_relative "imdb_cli/cli"
 require_relative "imdb_cli/scraper"
 require_relative "imdb_cli/movie"
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
-# module ImdbCli
-#   # Your code goes here...
-# end
